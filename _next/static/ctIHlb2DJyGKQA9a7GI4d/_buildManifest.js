b89417787ef271ca3385ffafbb54db8b4f2fcd4f
Navv6 <bwme3@naver.com>
2026-01-13 02:03:08 +0900

    Updates

--- _next/static/ctIHlb2DJyGKQA9a7GI4d/_buildManifest.js
+++ _next/static/ctIHlb2DJyGKQA9a7GI4d/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pages//_next/:path+",
+        "destination": "/pages/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
